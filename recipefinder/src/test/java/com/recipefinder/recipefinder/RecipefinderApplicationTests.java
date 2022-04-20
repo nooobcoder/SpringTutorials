791e0079bb356efbbaf7523ee96f29c692952f1d
Ankur Paul <suryashi2013@gmail.com>
2022-04-20 20:04:37 +0530

    LF line-endings

---
+++ recipefinder/src/test/java/com/recipefinder/recipefinder/RecipefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipefinder.recipefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipefinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
