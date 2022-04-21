bdff7e1845cd8db0e94c27308e2064aa496e5b8b
Ankur Paul <suryashi2013@gmail.com>
2022-04-21 19:31:43 +0530

    Add IndexController

---
+++ recipefinder/src/main/java/com/recipefinder/recipefinder/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.recipefinder.recipefinder.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"", "/", "index"})
+public class IndexController {
+
+    public String getIndexPage() {
+        return "index";
+    }
+}
