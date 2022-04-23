f57001cd9e480ab06c1054f3de507da5eee32dc0
Ankur Paul <suryashi2013@gmail.com>
2022-04-23 11:50:07 +0530

    Fix broken menu links
    Closes #30, closes #25
    
    Signed-off-by: Ankur Paul <suryashi2013@gmail.com>

--- SpringPetClinic/pet-clinic-web/src/main/java/com/example/springpetclinic/controllers/OwnerController.java
+++ SpringPetClinic/pet-clinic-web/src/main/java/com/example/springpetclinic/controllers/OwnerController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
@@ -15,10 +14,15 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"/", "/index", "/index.html"})
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index.html";
+    }
+
+    @RequestMapping({"/find", "/find/index", "/oups"})
+    public String findOwners() {
+        return "notimplemented";
     }
 }
