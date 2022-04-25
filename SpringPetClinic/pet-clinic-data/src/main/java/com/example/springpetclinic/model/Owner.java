a51531bac807b73817a53a3224ca6157f197e792
Ankur Paul <suryashi2013@gmail.com>
2022-04-25 09:08:09 +0530

    Convert Owners to JPA Entities.
    Closes nooobcoder/SpringTutorials #21,nooobcoder/springtutorials #14
    
    Signed-off-by: Ankur Paul <suryashi2013@gmail.com>

--- SpringPetClinic/pet-clinic-data/src/main/java/com/example/springpetclinic/model/Owner.java
+++ SpringPetClinic/pet-clinic-data/src/main/java/com/example/springpetclinic/model/Owner.java
@@ -1,13 +1,24 @@
 package com.example.springpetclinic.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
-
+    @Column(name = "address")
     private String address;
+    @Column(name = "city")
     private String city;
+    @Column(name = "telephone")
     private String telephone;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    @Column(name = "pets")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
--- SpringPetClinic/pet-clinic-data/src/main/java/com/example/springpetclinic/model/PetType.java
+++ SpringPetClinic/pet-clinic-data/src/main/java/com/example/springpetclinic/model/PetType.java
@@ -1,6 +1,13 @@
 package com.example.springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
+    @Column(name = "name")
     private String name;
 
     public String getName() {
