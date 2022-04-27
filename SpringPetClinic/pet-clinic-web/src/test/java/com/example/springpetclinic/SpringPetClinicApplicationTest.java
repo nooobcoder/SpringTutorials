1651eee809a39c1f033bd7831ab059be9869b867
Ankur Paul <suryashi2013@gmail.com>
2022-04-27 11:05:03 +0530

    Upgrade to JUnit 5
    Closes https://github.com/nooobcoder/SpringTutorials/issues/7, http://192.168.0.120/nooobcoder/springtutorials/-/issues/1
    
    Signed-off-by: Ankur Paul <suryashi2013@gmail.com>

---
+++ SpringPetClinic/pet-clinic-web/src/test/java/com/example/springpetclinic/SpringPetClinicApplicationTest.java
@@ -0,0 +1,21 @@
+package com.example.springpetclinic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class SpringPetClinicApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+
+    @Test
+    void contextLoads() {
+    }
+}
