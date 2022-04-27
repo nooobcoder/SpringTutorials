8365fefbba9e5f5f9810077d459a640e907e76a1
Ankur Paul <suryashi2013@gmail.com>
2022-04-27 09:54:26 +0530

    Add Test with Circle CI

---
+++ recipefinder/src/test/java/com/recipefinder/recipefinder/model/CategoryTest.java
@@ -0,0 +1,31 @@
+package com.recipefinder.recipefinder.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// Unit Test for Category
+class CategoryTest {
+    Category category;
+
+    @BeforeEach
+    public void setUp() {
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4l;
+        category.setId(4l);
+        assertEquals(4l, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipe() {
+    }
+}
