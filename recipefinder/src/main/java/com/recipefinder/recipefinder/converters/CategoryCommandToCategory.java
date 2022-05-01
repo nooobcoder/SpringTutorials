7b2b51aaf2e1fbe3f7901dbcc8f0e717ae35fdb3
Ankur Paul <suryashi2013@gmail.com>
2022-05-01 18:05:54 +0530

    Add Converters

---
+++ recipefinder/src/main/java/com/recipefinder/recipefinder/converters/CategoryCommandToCategory.java
@@ -0,0 +1,27 @@
+package com.recipefinder.recipefinder.converters;
+
+import com.recipefinder.recipefinder.commands.CategoryCommand;
+import com.recipefinder.recipefinder.model.Category;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryCommandToCategory implements Converter<CategoryCommand, Category> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Category convert(CategoryCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Category category = new Category();
+        category.setId(source.getId());
+        category.setDescription(source.getDescription());
+
+        return category;
+    }
+}
