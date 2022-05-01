416bb4f9eae75ed4a3d8ac18f01a1b9958fe94fd
Ankur Paul <suryashi2013@gmail.com>
2022-05-01 18:05:38 +0530

    Add Commmands

---
+++ recipefinder/src/main/java/com/recipefinder/recipefinder/commands/NotesCommand.java
@@ -0,0 +1,13 @@
+package com.recipefinder.recipefinder.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class NotesCommand {
+    private Long id;
+    private String recipeNotes;
+}
---
+++ recipefinder/src/main/java/com/recipefinder/recipefinder/commands/RecipeCommand.java
@@ -0,0 +1,23 @@
+package com.recipefinder.recipefinder.commands;
+
+import com.recipefinder.recipefinder.model.Difficulty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RecipeCommand {
+    private Long id;
+    private String description;
+    private Integer prepTime, cookTime, servings;
+    private String source, url, directions;
+    private Set<IngredientCommand> ingredients = new HashSet<>();
+    private Difficulty difficulty;
+    private NotesCommand notes;
+    private Set<CategoryCommand> categories = new HashSet<>();
+}
