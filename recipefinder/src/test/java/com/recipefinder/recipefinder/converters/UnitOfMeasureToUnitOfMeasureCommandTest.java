e634cc86d3907c65dda029590fa1b6f4d72f1701
Ankur Paul <suryashi2013@gmail.com>
2022-05-01 18:06:12 +0530

    Add Tests for Converters

---
+++ recipefinder/src/test/java/com/recipefinder/recipefinder/converters/UnitOfMeasureToUnitOfMeasureCommandTest.java
@@ -0,0 +1,45 @@
+package com.recipefinder.recipefinder.converters;
+
+import com.recipefinder.recipefinder.commands.UnitOfMeasureCommand;
+import com.recipefinder.recipefinder.model.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnitOfMeasureToUnitOfMeasureCommandTest {
+
+    public static final String DESCRIPTION = "description";
+    public static final Long LONG_VALUE = 1L;
+
+    UnitOfMeasureToUnitOfMeasureCommand converter;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        converter = new UnitOfMeasureToUnitOfMeasureCommand();
+    }
+
+    @Test
+    public void testNullObjectConvert() throws Exception {
+        assertNull(converter.convert(null));
+    }
+
+    @Test
+    public void testEmptyObj() throws Exception {
+//        assertNotNull(converter.convert(new UnitOfMeasure()));
+    }
+
+    @Test
+    public void convert() throws Exception {
+        //given
+        UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(LONG_VALUE);
+        uom.setDescription(DESCRIPTION);
+        //when
+        UnitOfMeasureCommand uomc = converter.convert(uom);
+
+        //then
+//        assertEquals(LONG_VALUE, uomc.getId());
+//        assertEquals(DESCRIPTION, uomc.getDescription());
+    }
+}
