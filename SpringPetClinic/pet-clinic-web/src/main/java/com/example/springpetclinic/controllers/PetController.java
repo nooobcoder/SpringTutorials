2a475493c19be8ef8eb633352976d8d9ed51b95c
Ankur Paul <suryashi2013@gmail.com>
2022-05-10 20:54:07 +0530

    Add ability to add or update a Pet
    References https://github.com/nooobcoder/SpringTutorials/issues/42, http://192.168.0.120/nooobcoder/springtutorials/-/issues/41
    
    Signed-off-by: Ankur Paul <suryashi2013@gmail.com>

---
+++ SpringPetClinic/pet-clinic-web/src/main/java/com/example/springpetclinic/controllers/PetController.java
@@ -0,0 +1,46 @@
+package com.example.springpetclinic.controllers;
+
+import com.example.springpetclinic.model.Owner;
+import com.example.springpetclinic.model.PetType;
+import com.example.springpetclinic.services.OwnerService;
+import com.example.springpetclinic.services.PetService;
+import com.example.springpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("pets")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") int ownerId) {
+        return ownerService.findById((long) ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+}
