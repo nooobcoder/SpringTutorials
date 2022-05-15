d78a54feca3a8da8939a63f227f2d098ebd31a75
Ankur Paul <suryashi2013@gmail.com>
2022-05-15 20:05:22 +0530

    Add test for IndexController
    Signed-off-by: Ankur Paul <suryashi2013@gmail.com>

---
+++ SpringPetClinic/pet-clinic-web/src/test/java/com/example/springpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,41 @@
+package com.example.springpetclinic.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+class IndexControllerTest {
+    @Mock
+    Model model;
+
+    @InjectMocks
+    IndexController controller;
+
+    @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
+    }
+
+    @Test
+    void testIndex() {
+        assertEquals("index", controller.index());
+    }
+
+    @Test
+    void testOupsHandler() {
+        assertEquals("notimplemented", controller.oupsHandler());
+    }
+}
