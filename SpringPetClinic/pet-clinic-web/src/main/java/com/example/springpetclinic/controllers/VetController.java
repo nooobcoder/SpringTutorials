2811f0947cafb02dc1369e3771a7f30d45d92b2f
Ankur Paul <suryashi2013@gmail.com>
2022-05-15 20:02:23 +0530

    Add VetController
    Signed-off-by: Ankur Paul <suryashi2013@gmail.com>

--- SpringPetClinic/pet-clinic-web/src/main/java/com/example/springpetclinic/controllers/VetController.java
+++ SpringPetClinic/pet-clinic-web/src/main/java/com/example/springpetclinic/controllers/VetController.java
@@ -1,24 +1,32 @@
 package com.example.springpetclinic.controllers;
 
+import com.example.springpetclinic.model.Vet;
 import com.example.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
 
-	private final VetService vetService;
+    private final VetService vetService;
 
-	VetController(VetService vetService) {
-		this.vetService = vetService;
-	}
+    VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
-	@GetMapping({ "/vets/index", "/vets/index.html", "/vets.html", "/vets" })
-	public String listVets(Model model) {
-		model.addAttribute("vets", vetService.findAll());
+    @GetMapping({"/vets/index", "/vets/index.html", "/vets.html", "/vets"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
 
-		return "vets/index";
-	}
+        return "vets/index";
+    }
 
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
+    }
 }
