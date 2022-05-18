fc8a3c90a11a82bd1be5b33ec8ca580f1d0460c3
Ankur Paul <suryashi2013@gmail.com>
2022-05-18 10:32:17 +0530

    Jacoco fixes

--- spring5-mvc-rest/src/main/java/guru/springfamework/api/v1/mapper/CustomerMapper.java
+++ spring5-mvc-rest/src/main/java/guru/springfamework/api/v1/mapper/CustomerMapper.java
@@ -3,11 +3,13 @@ package guru.springfamework.api.v1.mapper;
 import guru.springfamework.api.v1.model.CustomerDTO;
 import guru.springfamework.domain.Customer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CustomerMapper {
     CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
 
+    @Mapping(target = "customerUrl", source = "")
     CustomerDTO customerToCustomerDTO(Customer customer);
 }
