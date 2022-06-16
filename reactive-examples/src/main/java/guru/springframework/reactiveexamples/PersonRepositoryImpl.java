4cbf07e8976c975146201a533000109f695026f8
Ankur Paul <suryashi2013@gmail.com>
2022-06-16 21:02:00 +0530

    add reactive-examples

---
+++ reactive-examples/src/main/java/guru/springframework/reactiveexamples/PersonRepositoryImpl.java
@@ -0,0 +1,25 @@
+package guru.springframework.reactiveexamples;
+
+import guru.springframework.reactiveexamples.domain.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by jt on 2/27/21.
+ */
+public class PersonRepositoryImpl implements PersonRepository {
+    Person michael = new Person(1, "Michael", "Weston");
+    Person fiona = new Person(2, "Fiona", "Glenanne");
+    Person sam = new Person(3, "Sam", "Axe");
+    Person jesse = new Person(4, "Jesse", "Porter");
+
+    @Override
+    public Mono<Person> getById(Integer id) {
+        return findAll().filter(person -> person.getId().equals(id)).next();
+    }
+
+    @Override
+    public Flux<Person> findAll() {
+        return Flux.just(michael, fiona, sam, jesse);
+    }
+}
---
+++ reactive-examples/src/test/java/guru/springframework/reactiveexamples/ReactiveExamplesApplicationTest.java
@@ -0,0 +1,17 @@
+package guru.springframework.reactiveexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class ReactiveExamplesApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
