94cdf9a1ea6ae93b83712e3c2514b2c711fc3471
Ankur Paul <suryashi2013@gmail.com>
2022-06-21 21:35:11 +0530

    Fix CORS

--- jugtours-spring-app/jugtours/src/main/java/eth/nooobcoder/jugtours/controllers/GroupController.java
+++ jugtours-spring-app/jugtours/src/main/java/eth/nooobcoder/jugtours/controllers/GroupController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import eth.nooobcoder.jugtours.model.Group;
 import eth.nooobcoder.jugtours.repositories.GroupRepository;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api")
 class GroupController {
 
